24c15279a1a2da65e2df99651737074a438ea4ad
bossdmt0312 <122552032+bossdmt0312@users.noreply.github.com>
2023-03-13 17:31:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Day :");
+        int D = int.Parse(Console.ReadLine());
+        
+        Console.Write("Time :");
+        int K = int.Parse(Console.ReadLine());
+        if (D <= 1 i >= 7) {
+            
+        }
+            int m = D / 1 ;
+            int t = D / 2 ;
+            int w = D / 3 ;
+            int th = D / 4 ;
+            int f = D / 5 ;
+            int st = D / 6 ;
+            int su = D / 7 ;
+
+            Console.WriteLine(D);
+
+
+        }
+    }
